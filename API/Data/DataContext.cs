9e8c1f9f6512b076b2799af05d0588c163a5f93f
tearSorcerer <birdofhermestheherald@gmail.com>
2023-03-05 14:49:08 +0200

    End Of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.AppUser> Users { get; set; }
+    }
+}
