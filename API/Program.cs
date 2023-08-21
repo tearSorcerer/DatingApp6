e67472f7591505548e1983569fb7f668eed3d3d3
TearSorcerer <birdofhermestheherald@gmail.com>
2023-08-21 23:24:45 +0200

    Adding "sslmode=disable;" in an attempt to fix an issue

--- API/Program.cs
+++ API/Program.cs
@@ -34,9 +34,9 @@ else
         var pgPass = pgUserPass.Split(":")[1];
         var pgHost = pgHostPort.Split(":")[0];
         var pgPort = pgHostPort.Split(":")[1];
-	var updatedHost = pgHost.Replace("flycast", "internal");
+	    var updatedHost = pgHost.Replace("flycast", "internal");
 
-        connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+        connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=disable;";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
